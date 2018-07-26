bd0926be5ce93e8cf2d386132fdaa75cc4c40beb
Kousuke0320 <b1015013@gmail.com>
2018-07-26 17:04:25 +0900

    first commit

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <view class="container">
+    <view class="main">
+      <text-input />
+    </view>
+  </view>
+</template>
+ 
+<script>
+    import TextInput from './src/todo.vue';
+  export default {
+    components: {
+      TextInput
+    }
+  }
+</script>
+
+<style>
+.container {
+  background-color: #333;
+  flex: 1;
+  padding-top: 40px;
+  align-items: center;
+}
+.main {
+  flex: 1;
+  max-width: 400px;
+  align-items: center;
+}
+</style>
